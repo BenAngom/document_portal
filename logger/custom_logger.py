7214bbaffc473d4375eb68205a418a415da61946
benangom <benudhor@gmail.com>
2026-01-26 02:15:09 +0530

    Logging module updated

---
+++ logger/custom_logger.py
@@ -0,0 +1,33 @@
+import logging
+import os
+from datetime import datetime
+
+class CustomLogger:
+
+    def __init__(self, log_dir="logs"):
+        # Ensure logs directory exists
+        self.logs_dir = os.path.join(os.getcwd(), log_dir)
+        os.makedirs(self.logs_dir, exist_ok=True)
+
+        # Timestamped log file (for persistence)
+        log_file = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
+        log_file_path = os.path.join(self.logs_dir, log_file)
+        
+        #configure logging
+        logging.basicConfig(
+            filename=log_file_path,
+            format="[%(asctime)s: %(levelname)s: %(module)s]: %(message)s", 
+            level=logging.INFO,
+        )
+        
+    def get_logger(self, name=__file__):
+        return logging.getLogger(os.path.basename(name))
+    
+    
+    # # --- Usage Example ---
+if __name__ == "__main__":
+    logger = CustomLogger()
+    logger=logger.get_logger(__file__)
+    logger.info("Logger initialized successfully")
+    #logger.info("User uploaded a file", user_id=123, filename="report.pdf")
+    #logger.error("Failed to process PDF", error="File not found", user_id=123)
