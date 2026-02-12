32941bf1e5c14b585d7ce6dfd3ae57d37139b761
benangom <benudhor@gmail.com>
2026-02-13 03:16:41 +0530

    code for multi doc chat

---
+++ src/multi_document_chat/data_ingestion.py
@@ -0,0 +1,11 @@
+class DocumentIngestor:
+    def __init__(self):
+        pass
+
+    def ingest_files(self):
+        # Logic to ingest documents from the specified source
+        pass
+    
+    def _create_retriever(self, document):
+        # Logic to create a retriever for the ingested documents
+        pass
---
+++ src/multi_document_chat/retrieval.py
@@ -0,0 +1,24 @@
+class ConversationalRAG:
+    def __init__(self):
+        pass
+
+    def load_retriever_from_faiss(self):
+        # Logic to load a retriever from a FAISS index
+        pass
+    
+    def invoke(self):
+        # Logic to handle the conversational retrieval process
+        pass
+    
+    def _load_llm(self):
+        # Logic to load the language model for generating responses
+        pass
+    
+    @staticmethod
+    def _format_docs(self):
+        # Logic to format retrieved documents for the language model
+        pass
+    
+    def _build_lcel_chain(self):
+        # Logic to build a chain for language model execution
+        pass
