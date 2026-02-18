e1f103cd4beae69e7f6bdddcbe2153916ea7a4f0
benangom <benudhor@gmail.com>
2026-02-18 19:38:44 +0530

    retriver code updated

--- src/multi_document_chat/retrieval.py
+++ src/multi_document_chat/retrieval.py
@@ -52,7 +52,7 @@ class ConversationalRAG:
             self.retriever = vectorstore.as_retriever(search_type="similarity", session_kwargs={"k":5})  
             self.log.info("FAISS retriever loaded successfully", index_path=index_path, session_id=self.session_id)
             
-            self._build_lcel_chain()
+            ##self._build_lcel_chain()
             return self.retriever
             
         except Exception as e:
